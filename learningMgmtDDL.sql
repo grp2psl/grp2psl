96c9b159732a1e6e6900eec7a68b561ade46752a
Suneetptl <54058212+Suneetptl@users.noreply.github.com>
2021-07-30 20:22:20 +0530

    Add files via upload

---
+++ learningMgmtDDL.sql
@@ -0,0 +1,80 @@
+/*Drops grp2PSL user and learningMgmtDB database not required to run in initial iteration*/
+drop user 'grp2PSL'@'localhost';
+drop database learningMgmtDB;
+
+/*Creates new database named learningMgmtDB*/
+Create database learningMgmtDB;
+
+/*Creates new user named grp2PSL*/
+CREATE USER 'grp2PSL'@'localhost' IDENTIFIED BY 'grp2PSLPass';
+
+/*Grants all privileges on the created database to the created user*/
+GRANT ALL PRIVILEGES ON learningMgmtDB.* TO 'grp2PSL'@'localhost';
+
+/*Switches database to learningMgmtDB*/
+use learningMgmtDB;
+
+/*Creates table to store learner's data*/
+create table learner(
+	learnerid int,
+    name varchar(100),
+    department varchar(50),
+    phonenumber long,
+    email varchar(50),
+    password varchar(25),
+    primary key(learnerid)
+);
+
+/*Creates table trainer's data*/
+create table trainer(
+	trainerid int,
+    name varchar(100),
+    department varchar(50),
+    phonenumber long,
+    email varchar(50),
+    password varchar(25),
+    primary key(trainerid)
+);
+
+/*Creates table admin's data*/
+create table admin(
+	adminid int,
+    name varchar(100),
+    phonenumber long,
+    email varchar(50),
+    password varchar(25),
+    primary key(adminid)
+);
+
+/*Creates table to store course details*/
+create table course(
+	courseid int,
+    coursename varchar(100),
+    prerequisite varchar(300),
+    duration int,
+    primary key(courseid)
+);
+
+/*Create table to store course offering details*/
+create table courseoffering(
+	feedback varchar(150),
+    ratings int CHECK (ratings >= 0 AND ratings <= 5),
+    startdate date,
+    enddate date,
+	learnerid int,
+    tcid int,
+    status varchar(20),
+    percentage int check (percentage >= 0 AND percentage <= 100),
+    primary key(tcid),
+    foreign key(learnerid) references learner(learnerid)
+);
+
+/*Creates table which stores teachercourse mapping*/
+create table TeacherCourseMapping(
+	trainerid int,
+    courseid int,
+    tcid int,
+    primary key (tcid),
+    foreign key(trainerid) references trainer(trainerid),
+    foreign key(courseid) references course(courseid)
+)
