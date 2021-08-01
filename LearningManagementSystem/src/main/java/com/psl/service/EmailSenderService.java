7ee7292bda8c581fd3561b701e2d0ce65bc2b740
Vaishnavi <vaishnavi10march2000@gmail.com>
2021-08-01 12:34:04 +0530

    Added mail configuration

---
+++ LearningManagementSystem/src/main/java/com/psl/service/EmailSenderService.java
@@ -0,0 +1,24 @@
+package com.psl.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+	@Autowired
+	private JavaMailSender sender;
+	public void sendEmail(String fromEmail, String toEmail, String body, String subject) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(fromEmail);
+		//message.setFrom("group2.learning.management.system@gmail.com");
+		message.setTo(toEmail);
+		message.setText(body);
+		message.setSubject(subject);
+		sender.send(message);
+		System.out.println("Mail sent..");
+	}
+
+}
