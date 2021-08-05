c2ccdb3d7a70e99f2f2d5579a2cc93508f39320f
shreeyarajguru <shreeya_rajguru@persistent.com>
2021-08-05 18:15:12 +0530

    removed warnings

--- LearningManagementSystem/src/main/java/com/psl/controller/LearnerController.java
+++ LearningManagementSystem/src/main/java/com/psl/controller/LearnerController.java
@@ -1,13 +1,11 @@
 	package com.psl.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.psl.entities.CourseAttended;
 import com.psl.entities.Learner;
-import com.psl.entities.ScoreStatus;
 import com.psl.service.LearnerService;
 
 
--- LearningManagementSystem/src/main/java/com/psl/service/CourseOfferingService.java
+++ LearningManagementSystem/src/main/java/com/psl/service/CourseOfferingService.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
