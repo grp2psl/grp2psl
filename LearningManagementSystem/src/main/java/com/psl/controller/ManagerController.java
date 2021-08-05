eb274eed89a410c8df65c89ec47a4eec4e019851
shreeyarajguru <shreeya_rajguru@persistent.com>
2021-08-05 13:27:05 +0530

    Added comments for endpoint functionality documentation

--- LearningManagementSystem/src/main/java/com/psl/controller/ManagerController.java
+++ LearningManagementSystem/src/main/java/com/psl/controller/ManagerController.java
@@ -35,46 +35,77 @@ public class ManagerController {
 	@Autowired
 	private CourseOfferingService offeringService;
 		
+	/*
+	 * Get details of Manager by ID
+	 */
 	@GetMapping("/{id}")
 	public Manager getManager(@PathVariable int id) {
 		return service.getManager(id);
 	}
 	
+	/*
+	 * Add a manager
+	 */
 	@PostMapping("/register")
 	public void addManager(@RequestBody Manager m) {
 		service.addManager(m);
 	}
 	
+	/*
+	 * Enroll a leaner to a course
+	 * Request body contents :{leanerid, tcid, startdate, enddate}
+	 */
 	@PostMapping("/enroll-learner")
 	public void enrollLearner(@RequestBody CourseOffering offering) throws ParseException {
 		offeringService.enrollLearner(offering);
 	}
 	
+	/*
+	 * Enroll multiple learners to a course
+	 * RequestParam will be an excel file, for file contents use /generate-excel-enrolment
+	 */
 	@PostMapping("/enroll-learners")
 	public void enrollMultipleLearners(@RequestParam("file") MultipartFile csvFilePath) throws IOException, ParseException {
 		offeringService.enrollMultipleLearners(csvFilePath);
 	}
 	
+	/*
+	 * Update test scores of individual
+	 * Request body contents:{percentage}
+	 */
 	@PutMapping("/update-test-scores/{id}")
 	public void updateTestScore(@RequestBody CourseOffering offering, @PathVariable int id) {
 		offeringService.updateTestScore(id, offering.getPercentage());
 	}
 
+	/*
+	 * Update test scores of multiple
+	 * RequestParam will be an excel file, for file contents use /generate-excel-score-update
+	 */
 	@PutMapping("/update-test-scores")
 	public void updateMultipleTestScores(@RequestParam("file") MultipartFile csvFilePath) throws IOException, ParseException {
 		offeringService.updateMultipleTestScores(csvFilePath);
 	}
 	
+	/*
+	 * View all course offerings
+	 */
 	@GetMapping("/course-offerings")
 	public List<CourseOffering> viewCourseOfferings(){
 		return offeringService.viewCourseOfferings();
 	}
 	
+	/*
+	 * Delete a course offering by ID
+	 */
 	@DeleteMapping("/course-offering/{id}")
 	public void removeCourseOffering(@PathVariable int id) {
 		offeringService.removeCourseOffering(id);		
 	}
 	
+	/*
+	 * Download the excel format for multiple enrolment
+	 */
 	@GetMapping("/generate-excel-enrolment")
 	public void downloadFileFromLocalEnrolment() throws IOException {
 		Path file = Paths.get(System.getProperty("user.home"), "Downloads");
@@ -82,6 +113,9 @@ public class ManagerController {
 		System.out.println(file);
 	}	
 
+	/*
+	 * Download the excel format for updating multiple scores
+	 */
 	@GetMapping("/generate-excel-score-update")
 	public void downloadFileFromLocalScoreUpdate() throws IOException {
 		Path file = Paths.get(System.getProperty("user.home"), "Downloads");
