1432a860062b404869072e22060562d733c46ec4
Suneet <suneetptl@gmail.com>
2021-08-06 12:51:56 +0530

    Added inline documentation of ICourseOfferingDAO

--- LearningManagementSystem/src/main/java/com/psl/dao/ICourseOfferingDAO.java
+++ LearningManagementSystem/src/main/java/com/psl/dao/ICourseOfferingDAO.java
@@ -1,5 +1,10 @@
+/*
+ * Data Access Object Interface of Course Offering Entity
+ * Present in com.psl.dao package
+ */
 package com.psl.dao;
 
+//Necessary imports for DAO declaration
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,10 +12,18 @@ import org.springframework.data.repository.CrudRepository;
 import com.psl.entities.CourseOffering;
 import com.psl.entities.CourseOfferingId;
 
+/*
+ * Course Offering DAO interface extends CRUD (Create, Read ,Update, Delete) Repository
+ * CrudRepository has two parameters CourseOffering and CourseOfferingId
+ * CourseOffering is Entity Class on which CRUD operations are to be performed
+ * CourseOfferingId is primary key Class of CourseOffering Class
+ */
 public interface ICourseOfferingDAO extends CrudRepository<CourseOffering, CourseOfferingId>{
-
+	
+	//This function finds unique Course Offering and Learner Mapping using bpth tcId and learnerId
 	CourseOffering findByTcIdAndLearnerId(int tcId, int learnerId);
-
+	
+	//This function finds all Course Offerings in which given learner is enrolled by learnerId.
 	List<CourseOffering> findByLearnerId(int learnerId);
 	
 }
