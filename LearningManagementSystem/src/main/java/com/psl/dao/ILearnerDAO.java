44afe79c42a6be6ccda18b3f0d87351ad234075e
Suneet <suneetptl@gmail.com>
2021-08-06 12:48:03 +0530

    Added inline documentation of ILearnerDAO

--- LearningManagementSystem/src/main/java/com/psl/dao/ILearnerDAO.java
+++ LearningManagementSystem/src/main/java/com/psl/dao/ILearnerDAO.java
@@ -1,8 +1,19 @@
+/*
+ * Data Access Object Interface of Learner Entity
+ * Present in com.psl.dao package
+ */
 package com.psl.dao;
 
+//Necessary imports for DAO declaration
 import org.springframework.data.repository.CrudRepository;
 
 import com.psl.entities.Learner;
 
+/*
+ * Learner DAO interface extends CRUD (Create, Read ,Update, Delete) Repository
+ * CrudRepository has two parameters Learner and Integer
+ * Learner is Entity Class on which CRUD operations are to be performed
+ * Integer is data type of primary key of Learner Class
+ */
 public interface ILearnerDAO extends CrudRepository<Learner,Integer>{
 }
