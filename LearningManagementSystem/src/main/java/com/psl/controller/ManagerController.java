ff4f297fe82757aeb55895c3a07fbbcc88e6183e
vaishnavipatil555 <55610550+vaishnavipatil555@users.noreply.github.com>
2021-08-06 13:45:29 +0530

    Added CrossOrigin

--- LearningManagementSystem/src/main/java/com/psl/controller/ManagerController.java
+++ LearningManagementSystem/src/main/java/com/psl/controller/ManagerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,6 +31,7 @@ import com.psl.service.ManagerService;
 
 @RestController
 @RequestMapping("/managers")
+@CrossOrigin(origins="http://localhost:3000")
 public class ManagerController {
 	@Autowired
 	private ManagerService service;
