431ed4551793208bd82724e94d6d953dd45c4399
Suneet <suneetptl@gmail.com>
2021-08-06 13:10:26 +0530

    Added inline documentation of CourseOfferingService

--- LearningManagementSystem/src/main/java/com/psl/service/CourseOfferingService.java
+++ LearningManagementSystem/src/main/java/com/psl/service/CourseOfferingService.java
@@ -1,5 +1,9 @@
+/*
+ * Declaration of Course Offering Service which completes various requests on CourseOffering class.
+ */
 package com.psl.service;
 
+//Required imports for the service declaration.
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +12,22 @@ import org.springframework.stereotype.Service;
 import com.psl.dao.ICourseOfferingDAO;
 import com.psl.entities.CourseOffering;
 
+//This annotation enables use of this class as a service named courseOfferingService.
 @Service("courseOfferingService")
 public class CourseOfferingService {
 	
+	//Autowires the courseOffering Service with courseOffering DAO interface.
 	@Autowired
 	private ICourseOfferingDAO dao;
 	
+	//Function to allow Learner to given feedback on given course Offering
 	public void AddFeedback(int learnerId, int tcId, String feedback) {
 		CourseOffering co = dao.findByTcIdAndLearnerId(tcId, learnerId);
 		co.setFeedback(feedback);
 		dao.save(co);
 	}
 	
+	//Lists all Course Offerings of given learner which is identified by learnerId 
 	public List<CourseOffering> getCourseOfferings(int learnerId) {
 		return dao.findByLearnerId(learnerId);
 	}
