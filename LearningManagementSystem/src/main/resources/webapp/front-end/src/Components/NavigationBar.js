0f4aa3c2dcc0f08f67e18075e63f4c1cfdf0f524
Vaishnavi <vaishnavi10march2000@gmail.com>
2021-08-06 23:03:29 +0530

    Added buttons in navbar and changed bg-color

--- LearningManagementSystem/src/main/resources/webapp/front-end/src/Components/NavigationBar.js
+++ LearningManagementSystem/src/main/resources/webapp/front-end/src/Components/NavigationBar.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import {Link} from 'react-router-dom'
+import {Navbar, Nav, Container, Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faHome,
+    faUser,
+    faSignOutAlt
+  } from "@fortawesome/free-solid-svg-icons";
 
 class NavigationBar extends React.Component{
     render(){
@@ -12,13 +18,28 @@ class NavigationBar extends React.Component{
     				<Link to={""} className='navbar-brand'>
                         Learning Manager
                     </Link>
-               
-                    <Nav className="me-auto">
-                        <Link to={""} className='nav-link'>Home</Link>
-                        <Link to={""} className='nav-link'>My Profile</Link>
-                    </Nav>
                     <Nav>
-                        <Link to={""} className='nav-link'>Logout</Link>
+                        <Link to={""} className='nav-link'>
+                            <Button
+                                size="md"
+                                variant="outline-success"
+                            >
+                            <FontAwesomeIcon icon={faHome} />
+                            </Button>{" "}</Link>
+                        <Link to={""} className='nav-link'>
+                            <Button
+                                size="md"
+                                variant="outline-primary"
+                            >
+                            <FontAwesomeIcon icon={faUser} />
+                            </Button>{" "}</Link>
+                        <Link to={""} className='nav-link'>
+                            <Button
+                                size="md"
+                                variant="outline-danger"
+                            >
+                            <FontAwesomeIcon icon={faSignOutAlt} />
+                            </Button>{" "}</Link>
                     </Nav>
                     </Container>                
                 </Navbar>
