e66205c0958126a5d08f054787e02a1742a6e654
vaishnavipatil555 <55610550+vaishnavipatil555@users.noreply.github.com>
2021-08-06 13:44:37 +0530

    Added CrossOrigin

--- LearningManagementSystem/src/main/java/com/psl/controller/TrainerController.java
+++ LearningManagementSystem/src/main/java/com/psl/controller/TrainerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,6 +22,7 @@ import com.psl.service.TrainerService;
 
 @RestController
 @RequestMapping("/trainers")
+@CrossOrigin(origins="http://localhost:3000")
 public class TrainerController {
 	@Autowired
 	private TrainerService service;
