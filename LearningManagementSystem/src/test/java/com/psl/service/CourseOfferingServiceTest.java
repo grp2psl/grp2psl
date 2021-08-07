2768122da78d7993c69d979e87eef45e944ae8b3
shreeyarajguru <shreeya_rajguru@persistent.com>
2021-08-07 19:58:35 +0530

    Added test cases for view functions and generate file functions

--- LearningManagementSystem/src/test/java/com/psl/service/CourseOfferingServiceTest.java
+++ LearningManagementSystem/src/test/java/com/psl/service/CourseOfferingServiceTest.java
@@ -1,6 +1,7 @@
 package com.psl.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -8,6 +9,7 @@ import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.text.ParseException;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -18,7 +20,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -37,12 +38,12 @@ public class CourseOfferingServiceTest {
 	@Test
 	@Order(1)
 	public void testEnrollLearner() throws ParseException, JsonProcessingException, JsonMappingException{
-		int id = service.getMaxId();
+		int id = service.getMaxId(); //implicit testing of getMaxId()
 		String request = "{\"learnerid\":12,\"tcid\":1,\"startdate\":\"2021-09-01\",\"enddate\":\"2021-09-05\"}";
 		ObjectMapper mapper = new ObjectMapper();
 		CourseOffering offering = mapper.readValue(request, CourseOffering.class);
 		service.enrollLearner(offering);
-		CourseOffering result = service.getCourseOffering(service.getMaxId());
+		CourseOffering result = service.getCourseOffering(service.getMaxId()); //implicit testing of getCourseOffering(int id)
 		assertThat(result.getLearnerid()).isEqualTo(12);
 		assertThat(result.getTcid()).isEqualTo(1);
 		assertThat(result.getCourseofferingid()).isEqualTo(id+1);
@@ -56,7 +57,7 @@ public class CourseOfferingServiceTest {
 		assertThat(result.getLearnerid()).isEqualTo(12);
 		assertThat(result.getPercentage()).isEqualTo(67);
 		assertThat(result.getFeedback()).isEqualTo(null);
-		assertThat(result.getStatus()).isEqualTo("FAIL,FEEDBACK_PENDING");
+		assertThat(result.getStatus()).isEqualTo("FAIL,FEEDBACK_PENDING"); //implicit testing of updateCourseOfferingStatus(CourseOffering offering)
 	}
 	
 	@Test
@@ -109,5 +110,39 @@ public class CourseOfferingServiceTest {
 		CourseOffering offering = service.getCourseOffering((int)row.getCell(0).getNumericCellValue());
 		double percentage = (double)row.getCell(1).getNumericCellValue();
 	    assertThat(offering.getPercentage()).isEqualTo(percentage);
+	}
+	
+	@Test
+	@Order(7)
+	public void testViewTrainerDetails() {
+		Map<String, Object> response = service.viewTrainerDetails(1);
+		assertThat(response.get("trainerDetails")).hasFieldOrPropertyWithValue("trainerid", 1);
+		assertThat(response).hasFieldOrProperty("courses");
+		assertThat(response).hasFieldOrProperty("offerings");
+	}
+	
+	@Test
+	@Order(8)
+	public void testViewCourseDetailsByTrainerId() {
+		Map<String, Object> response = service.viewCourseDetailsByTrainerId(1, 2);
+		assertThat(response.get("courseDetails")).hasFieldOrPropertyWithValue("courseid", 2);
+		assertThat(response).hasFieldOrProperty("avgRating");
+		assertThat(response).hasFieldOrProperty("offerings");
+	}
+	
+	@Test
+	@Order(9)
+	public void testGenerateExcelForEnrolment() throws IOException {
+		service.generateExcelForEnrolment("downloads/");
+		Path file = Paths.get("downloads/enrollLearners.xlsx");
+		assertTrue(Files.exists(file));
+	}
+	
+	@Test
+	@Order(10)
+	public void testGenerateExcelForScoreUpdate() throws IOException {
+		service.generateExcelForScoreUpdate("downloads/");
+		Path file = Paths.get("downloads/updateScores.xlsx");
+		assertTrue(Files.exists(file));
 	}
 }
