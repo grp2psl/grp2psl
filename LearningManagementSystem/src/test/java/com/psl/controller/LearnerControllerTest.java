4b6c3eba5a021e5f8ed96932ac057c6484928386
dakshgupta600000 <daksh_gupta@gmail.com>
2021-08-11 11:02:46 +0530

    testin trainer2 1

---
+++ LearningManagementSystem/src/test/java/com/psl/controller/LearnerControllerTest.java
@@ -0,0 +1,29 @@
+package com.psl.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.psl.service.LearnerService;
+
+@WebMvcTest(LearnerController.class)
+public class LearnerControllerTest {
+	@Autowired
+	MockMvc mvc;
+	@MockBean
+	LearnerService service;
+	@Test
+	public void test_home() throws Exception{
+		this.mvc.perform(get("/learners/test")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+		
+	}
+
+}
---
+++ LearningManagementSystem/src/test/java/com/psl/service/CourseServiceTest.java
@@ -0,0 +1,48 @@
+package com.psl.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.psl.entities.Course;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class CourseServiceTest {
+	@Autowired
+	CourseService service;
+	
+	@Test
+	@Order(1)
+	public void testListProducts() {
+	    List<Course> courses = (List<Course>) service.findAll();
+	    assertThat(courses).size().isGreaterThan(0);
+	}
+	
+	@Test
+	@Order(2)
+	public void testAddCourse() {
+		Course c=new Course(369,"C++","Basic programming","OOPs concepts","8 hr");
+	    service.addCourse(c);
+	     
+	    Course course=service.getCourse(369);
+	    assertThat(course.getCourseid()).isEqualTo(369);
+	    
+	}
+	
+	@Test
+	@Order(3)
+	public void testGetCourse() {
+	    Course course=service.getCourse(2);
+	    assertThat(course.getCourseid()).isEqualTo(2);
+	    
+	}
+	
+}
