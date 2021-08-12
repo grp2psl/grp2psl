e658f8938f44aef7a34a7040aee5a849e49ea356
shreeyarajguru <shreeya_rajguru@persistent.com>
2021-08-12 20:29:16 +0530

    removed unused import @IdClass

--- LearningManagementSystem/src/main/java/com/psl/entities/CourseOffering.java
+++ LearningManagementSystem/src/main/java/com/psl/entities/CourseOffering.java
@@ -11,7 +11,6 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
