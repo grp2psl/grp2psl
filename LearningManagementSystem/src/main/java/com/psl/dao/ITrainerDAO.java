404d7a55434a7e11f75aad5c2c77604bf4c285d1
Vaishnavi <vaishnavi10march2000@gmail.com>
2021-08-14 12:38:51 +0530

    Removed public from interfaces

--- LearningManagementSystem/src/main/java/com/psl/dao/ITrainerDAO.java
+++ LearningManagementSystem/src/main/java/com/psl/dao/ITrainerDAO.java
@@ -13,18 +13,19 @@ import com.psl.entities.Trainer;
 
 public interface ITrainerDAO extends CrudRepository<Trainer, Integer>{
 	
-	public Trainer findByTrainerId(int id);
+	Trainer findByTrainerId(int id);
+	
 	/*
 	 * CUSTOM QUERY TO HIDE PASSWORD
 	 */
 	@Query(value = "select trainerid, name, department, email, phonenumber, password=null as password from trainer", nativeQuery = true)
-	public List<Trainer> findAll();
+	List<Trainer> findAll();
 
 	/*
 	 * AUTO-INCREMENT ID
 	 */
 	@Query(value="select max(trainerid) + 1 from trainer", nativeQuery=true)
-	public Integer getNextId();
+	Integer getNextId();
 
 	/*
 	 * CUSTOM QUERY FOR NEW RECORD INSERTION  
@@ -32,7 +33,7 @@ public interface ITrainerDAO extends CrudRepository<Trainer, Integer>{
 	@Transactional
 	@Modifying
 	@Query(value="insert into trainer(trainerid, name, department, phonenumber, email, password) values(?1, ?2, ?3, ?4, ?5, ?6)", nativeQuery=true)
-	public void saveNewEntry(@Param("id") int id, @Param("name") String name, @Param("department") String department, 
+	void saveNewEntry(@Param("id") int id, @Param("name") String name, @Param("department") String department, 
 			@Param("phoneNumber") String phoneNumber, @Param("email") String email, @Param("password") String password);
 
 	/*
@@ -41,6 +42,6 @@ public interface ITrainerDAO extends CrudRepository<Trainer, Integer>{
 	@Transactional
 	@Modifying
 	@Query(value="update trainer set department = ?1, phonenumber = ?2 where trainerid = ?3", nativeQuery=true)
-	public void updateEntry(@Param("department") String department, 
+	void updateEntry(@Param("department") String department, 
 			@Param("phoneNumber") String phoneNumber, @Param("id") int id);
 }
