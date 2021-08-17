d678629aa9cecd0d17da31f92713b117dcc58ee9
Vaishnavi <vaishnavi10march2000@gmail.com>
2021-08-17 12:33:43 +0530

    Removed warnings

--- LearningManagementSystem/src/test/java/com/psl/LearnerTests.java
+++ LearningManagementSystem/src/test/java/com/psl/LearnerTests.java
@@ -19,10 +19,8 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
--- LearningManagementSystem/src/test/java/com/psl/service/LearnerServiceTest.java
+++ LearningManagementSystem/src/test/java/com/psl/service/LearnerServiceTest.java
@@ -34,7 +34,6 @@ import com.psl.entities.Learner;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.psl.entities.CourseAttended;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
